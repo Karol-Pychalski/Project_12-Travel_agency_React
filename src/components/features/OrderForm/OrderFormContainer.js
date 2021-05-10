dfa8eb6a256997215be283067014ee28fb2af34e
karol Pychalski <k.p201x@gmail.com>
2021-05-10 12:00:06 +0200

    Task 14.3 completed

--- src/components/features/OrderForm/OrderFormContainer.js
+++ src/components/features/OrderForm/OrderFormContainer.js
@@ -1,9 +1,15 @@
 import {connect} from 'react-redux';
-import {getOrderOptions} from '../../../redux/orderRedux.js';
+import {getOrderOptions, setOrderOption} from '../../../redux/orderRedux.js';
 import OrderForm from './OrderForm.js';
 
 const mapStateToProps = (state) => ({
   options: getOrderOptions(state),
 });
 
-export default connect(mapStateToProps)(OrderForm);
+//z zad. 14.3: mapowanie dispatchera
+const mapDispatchProps = (dispatch) => ({
+  setOrderOption: payload => dispatch(setOrderOption(payload)),
+});
+
+export default connect(mapStateToProps, mapDispatchProps)(OrderForm);
+
